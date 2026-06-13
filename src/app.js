9808bc9ff763dc333bd473cf57e3b04bd737ffe3
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-13 17:07:57 +0530

    Exploring GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
-  
+const PORT = 3000; 
 // Request handler...
-app.use('/hello',(req,res)=>{
-    res.end("Hello from the server");
-})
+// this will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Adarsh",
+        lastName:"Pandey"
+    });
+});
+
+app.post("/user",(req,res)=>{
+    // saving data to DB
+    res.send("data successfully saved to the database");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("Deleted Successfully");
+});
+
+// this will match all the HTTP method API call to /hello
+app.use("/hello",(req,res)=>{
+    res.send("Hello from the server");
+});
 
 app.listen(PORT,()=>{
     console.log(`server is listening on port ${PORT}`);
