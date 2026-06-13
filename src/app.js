0ed5a1edd027451cf9651639f794169e90ffbab1
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-13 17:23:11 +0530

    Understanding the routes

--- src/app.js
+++ src/app.js
@@ -4,25 +4,21 @@ const app = express();
 const PORT = 3000; 
 // Request handler...
 // this will only handle GET call to /user
-app.get("/user",(req,res)=>{
+
+// app.get("/user",(req,res)=>{
+//     console.log(req.query);
+//     res.send({firstName: "Adarsh",
+//         lastName:"Pandey"
+//     });
+// });
+
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstName: "Adarsh",
         lastName:"Pandey"
     });
 });
 
-app.post("/user",(req,res)=>{
-    // saving data to DB
-    res.send("data successfully saved to the database");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Successfully");
-});
-
-// this will match all the HTTP method API call to /hello
-app.use("/hello",(req,res)=>{
-    res.send("Hello from the server");
-});
 
 app.listen(PORT,()=>{
     console.log(`server is listening on port ${PORT}`);
