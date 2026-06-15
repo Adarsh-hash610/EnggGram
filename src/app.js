fdcddc9301011e1882671e3ca5117bf90f64df74
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-15 12:51:16 +0530

    GET APIs

--- src/app.js
+++ src/app.js
@@ -20,6 +20,42 @@ app.post('/signup',async(req,res)=>{
     }
 });
 
+app.get('/user', async(req,res)=>{
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId:userEmail});
+        if(users.length === 0)
+        {
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err)
+    {
+        res.status(400).send("Something went wrong!!");
+    }
+});
+
+// Feed API - GET/feed - get all the user from the database
+app.get('/feed',async (req,res)=>{
+    try {
+        const users = await User.find({});
+        if(users.length === 0)
+        {
+            res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+        
+    } catch (err) {
+        res.status(400).send("something went wrong");
+        
+    }
+})
 
 
 connectDB()
