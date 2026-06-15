3d1caacb4e980ba3cfca45c34f0a8b37aa2f220f
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-15 14:10:11 +0530

    Update using PATCH API

--- src/app.js
+++ src/app.js
@@ -69,6 +69,17 @@ app.delete('/user', async (req,res)=>{
     }
 });
 
+app.patch('/user', async (req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try {
+        const user = await User.findByIdAndUpdate({_id:userId},data)
+        res.send("User updated successfully");
+    } catch (err) {
+        res.status(400).send("something went wrong!!")        
+    }
+});
+
 
 connectDB()
 .then(()=>{
