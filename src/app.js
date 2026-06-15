3d6a30cf9d222ebe3f60b51d94e5d5147aa2d29a
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-15 15:56:24 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -69,10 +69,28 @@ app.delete('/user', async (req,res)=>{
     }
 });
 
-app.patch('/user', async (req,res)=>{
-    const userId = req.body.userId;
+app.patch('/user/:userId', async (req,res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
+
     try {
+        const ALLOWED_UPDATES = [
+            // userId,
+            "photoUrl",
+            "about",
+            "gender",
+            "age",
+            "skills",
+        ];
+        const isUpdateAllowed = Object.keys(data).every((k)=>
+            ALLOWED_UPDATES.includes(k));
+
+        if(!isUpdateAllowed){
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length >10){
+            throw new Error("Skill Limit exceeded!!");
+        }
         const user = await User.findByIdAndUpdate({_id:userId},data,{
             returnDocument:"after",
             runValidators:true,
