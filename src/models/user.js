fca20f6128089aae344ef5663adef44a545649dc
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-15 16:17:01 +0530

    Validations at API and DB level

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -14,13 +15,24 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("invalid email address: "+value);
+            }
+        },
     },
     password:{
         type:String,
         required: true,
         minLength: 8,
-        trim: true
+        trim: true,
+        validate(value){
+            if(!validator.isStrongPassword(value))
+            {
+                throw new Error("Entered Password is weak: "+value);    
+            }
+        }
     },
     age:{
         type:Number,
@@ -35,7 +47,12 @@ const userSchema = new mongoose.Schema({
         }
     },
     photoUrl:{
-        type: String
+        type: String,
+        validate(value){
+            if(!validator.isURL(value)){
+                throw new Error("Invalid photo URL: "+value);
+            }
+        }
     },
     about:{
         type: String,
