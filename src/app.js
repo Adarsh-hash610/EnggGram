3861649651485e93e86c2059cbd404c5c79488d8
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-15 15:26:32 +0530

    Schema Validation

--- src/app.js
+++ src/app.js
@@ -73,10 +73,14 @@ app.patch('/user', async (req,res)=>{
     const userId = req.body.userId;
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate({_id:userId},data)
+        const user = await User.findByIdAndUpdate({_id:userId},data,{
+            returnDocument:"after",
+            runValidators:true,
+        });
+        console.log(user);
         res.send("User updated successfully");
     } catch (err) {
-        res.status(400).send("something went wrong!!")        
+        res.status(400).send("something went wrong!!"+err.message);        
     }
 });
 
