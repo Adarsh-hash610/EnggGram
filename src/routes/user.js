56ee106a6c0298a89385a1ba9ea175fe51061f59
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-06-18 13:30:46 +0530

    adding feed api for viewing the people

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const {userAuth} = require('../middlewares/auth');
 const ConnectionRequest = require('../models/connectionRequest');
 const { connection } = require('mongoose');
+const User = require('../models/user');
 
 // get all the pending request for the loggedIn user...
 userRouter.get('/user/requests/received', userAuth, async(req,res)=>{
@@ -50,6 +51,37 @@ userRouter.get('/user/connections', userAuth, async(req,res)=>{
         res.json({data});
     } catch (err) {
         res.status(400).send("ERROR: "+err.message);
+    }
+});
+
+userRouter.get('/user/feed', userAuth, async(req,res)=>{
+    try {
+        const loggedInUser = req.user;
+
+        const connectionRequest = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser._id},
+                {toUserId: loggedInUser._id}
+            ],
+        }).select("fromUserId toUserId");
+
+        const hideUserFromFeed = new Set();
+        connectionRequest.forEach((req)=>{
+            hideUserFromFeed.add(req.fromUserId.toString());
+            hideUserFromFeed.add(req.toUserId.toString());
+        });
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUserFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ],
+        }).select("firstName lastName, about, skills");
+
+        res.send(users);
+        
+    } catch (err) {
+        res.status(400).send("ERROR: "+err.message); 
     }
 })
 module.exports = userRouter;
