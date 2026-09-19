053772a3a2dd14f3e1dcf99cb5312444dd568c84
Adarsh-hash610 <pandeyadarsh639@gmail.com>
2026-09-19 20:27:21 +0530

    Cors and axios setup

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const connectDB = require('./config/database');
 const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 3000; 
+const cors = require('cors');
 
+app.use(
+    cors({
+        origin:"http://localhost:5173/",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
